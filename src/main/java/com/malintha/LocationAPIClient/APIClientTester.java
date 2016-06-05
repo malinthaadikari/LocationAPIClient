2269baf5cd8e9617f7ee0489a0868dd018316b17
Malintha <malintha@wso2.com>
2016-06-06 02:57:10 +0530

    Did improvements

--- src/main/java/com/malintha/LocationAPIClient/APIClientTester.java
+++ src/main/java/com/malintha/LocationAPIClient/APIClientTester.java
@@ -7,6 +7,9 @@ package com.malintha.LocationAPIClient;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
 
 /*
 This is the main class. Users should provide the city name as the first command line argument with execution
@@ -16,7 +19,7 @@ public class APIClientTester {
 
     private final static Log LOGGER = LogFactory.getLog(APIClientTester.class);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException, JSONException {
 
         String response;
         PropertyReader propertyReader = new PropertyReader();
@@ -24,7 +27,7 @@ public class APIClientTester {
 
         //handling the case when user doesn't provide the city name
         if (args.length == 0) {
-            LOGGER.error("City name has not been provided");
+            LOGGER.error("City name has not been provided. Please re-run with a city name");
         } else {
             response = apiClient.getLocationInfo(args[0]);
             if (new JSONArray(response).length() == 0) {
